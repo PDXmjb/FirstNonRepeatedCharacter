6503a9fb897ec948681ab5d8bf268850542457e6
Mike Brooks <PDXmjb@users.noreply.github.com>
2018-01-31 17:04:49 -0800

    Detects and returns the first character in a string that is not repeated within the string.
    Write a function named firstNonRepeatingLetter† that takes a string input, and returns the first character that is not repeated anywhere in the string.
    
    For example, if given the input 'stress', the function should return 't', since the letter t only occurs once in the string, and occurs first in the string.
    
    As an added challenge, upper- and lowercase letters are considered the same character, but the function should return the correct case for the initial letter. For example, the input 'sTreSS' should return 'T'.
    
    If a string contains all repeating characters, it should return the empty string ("").
    
    † Note: the function is called firstNonRepeatingLetter for historical reasons, but your function should handle any Unicode character.

---
+++ FirstNonRepeatedCharacter/FirstNonRepeatingLetterClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class FirstNonRepeatingLetterClass
+{
+    public static string FirstNonRepeatingLetter(string s)
+    {
+        var compare = s.ToUpper();
+        for (int i = 0; i < s.Length; i++)
+        {
+            var c = compare[i];
+
+            // If the first and last instance of the character are the same,
+            // it is the only instance of that letter or character.
+            if (compare.IndexOf(c) == compare.LastIndexOf(c))
+            {
+                return s[i].ToString();
+            }
+        }
+
+        return String.Empty;
+    }
+}
